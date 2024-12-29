77dcb9fb85ee8f909c9a8620a05687ce2f5fbae3
DAnaylxt <170872525+DAnaylxt@users.noreply.github.com>
2024-12-29 11:02:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector(".hamburger");
+const navlist =  document.querySelector(".nav-list");
+
+if(hamburger){
+hamburger.addEventListener("click", () => {
+    navlist.classList.toggle("open");
+});
+}
+
+
+// Popup
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup-close");
+
+if(popup){
+    closePopup.addEventListener("click", () => {
+        popup.classList.add("hide-popup");
+    });
+
+
+    window.addEventListener("load", () => {
+        setTimeout( () => {
+            popup.classList.remove("hide-popup");
+        }, 100)
+    })
+}
